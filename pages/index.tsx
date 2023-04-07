7c32ec1031ac2b180ca1397d26bf45476847a135
Doma <leishenghao@126.com>
2023-04-07 16:23:11 +0800

    wip website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,166 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Button, Input, Slider } from "rsuite";
 
-const inter = Inter({ subsets: ['latin'] })
+function useImage(src: string) {
+  const [image, setImage] = useState<HTMLImageElement | undefined>(undefined);
+
+  useEffect(() => {
+    if (typeof Image === "undefined") return;
+    const image = new Image();
+    image.onload = () => {
+      console.log(image.width, image.height);
+      setImage(image);
+    };
+    image.src = src;
+  }, [src]);
+
+  return image;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [username, setUsername] = useState("SevenOutman");
+  const [org, setOrg] = useState("rsuite");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [orgAvatarSize, setOrgAvatarSize] = useState(30);
+  const [hPosition, setHPosition] = useState(35);
+  const [vPosition, setVPosition] = useState(100);
+
+  const hOffset = ((100 - orgAvatarSize) * hPosition) / 100;
+  const vOffset = ((100 - orgAvatarSize) * vPosition) / 100;
+
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const userImage = useImage(`https://github.com/${username}.png`);
+  const orgImage = useImage(`https://github.com/${org}.png`);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+
+      if (ctx) {
+        ctx.clearRect(0, 0, 500, 500);
+
+        if (userImage) {
+          ctx.drawImage(userImage, 0, 0, 500, 500);
+        }
+
+        if (orgImage) {
+          const hOffset = (((100 - orgAvatarSize) * hPosition) / 100) * 5;
+          const vOffset =
+            500 -
+            orgAvatarSize * 5 -
+            (((100 - orgAvatarSize) * vPosition) / 100) * 5;
+
+          ctx.drawImage(
+            orgImage,
+            hOffset,
+            vOffset,
+            orgAvatarSize * 5,
+            orgAvatarSize * 5
+          );
+        }
+      }
+    }
+  }, [hPosition, orgAvatarSize, orgImage, userImage, vPosition]);
+
+  return (
+    <main className="min-h-screen p-24">
+      <h1>Org-Badged Avatar</h1>
+
+      <Input
+        type="text"
+        value={username}
+        onChange={setUsername}
+        style={{ display: "inline-block", width: "unset" }}
+      />
+
+      <Input
+        type="text"
+        value={org}
+        onChange={setOrg}
+        style={{ display: "inline-block", width: "unset" }}
+      />
+      <canvas
+        ref={canvasRef}
+        width={500}
+        height={500}
+        style={{ width: 250, height: 250 }}
+      />
+
+      <div>
+        <div className="flex">
+          <div
+            ref={divRef}
+            className="relative rounded-full overflow-hidden"
+            style={{ width: 250, height: 250 }}
+          >
+            {username ? (
+              <img
+                src={`https://github.com/${username}.png`}
+                alt=""
+                className="block h-full w-full select-none pointer-events-none"
+              />
+            ) : null}
+
+            <div
+              className="absolute bg-white rounded overflow-hidden"
+              style={{
+                width: `${orgAvatarSize}%`,
+                height: `${orgAvatarSize}%`,
+                left: `${hOffset}%`,
+                bottom: `${vOffset}%`,
+              }}
+            >
+              {org ? (
+                <img
+                  src={`https://github.com/${org}.png`}
+                  alt=""
+                  className="w-full h-full select-none pointer-events-none"
+                />
+              ) : null}
+            </div>
+          </div>
+
+          <Slider
+            value={vPosition}
+            onChange={setVPosition}
+            min={0}
+            max={100}
+            vertical
+            style={{ height: 200 }}
+          />
+        </div>
+        <Slider
+          value={hPosition}
+          onChange={setHPosition}
+          min={0}
+          max={100}
+          style={{ width: 200 }}
         />
       </div>
+      <Slider
+        value={orgAvatarSize}
+        onChange={setOrgAvatarSize}
+        min={0}
+        max={100}
+        style={{ width: 200 }}
+      />
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Button
+        appearance="primary"
+        onClick={() => {
+          const canvas = canvasRef.current;
+          const dataUrl = canvas?.toDataURL();
+          const link = document.createElement("a");
+          link.download = "my-image-name.png";
+          link.href = dataUrl;
+          link.click();
+        }}
+      >
+        Save Image
+      </Button>
     </main>
-  )
+  );
 }
